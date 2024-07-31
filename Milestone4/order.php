3e0f38af992448dabcd3fbf45edb5a3f7af36570
janapchi <janapchi@students.cs.ubc.ca>
2024-07-30 22:28:50 -0700

    testing intellij push with cwl id

--- Milestone4/order.php
+++ Milestone4/order.php
@@ -1,7 +1,6 @@
 <html>
-
 	<?php
-		echo "Hello world";
+		echo "Test";
 		phpinfo(); // Print PHP version and config info
 		// can use echo ""; to format html within php
 		// reminder make a db access file (???)
