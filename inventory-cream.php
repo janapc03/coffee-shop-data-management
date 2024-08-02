9ee273a7e47268da8d2e85ebeedbf1593986b30a
Mandy Deng <miaodan@students.cs.ubc.ca>
2024-08-02 11:16:59 -0700

    testing to connect to coffeeShop db, listRecipes display tuples connects to the coffee table in coffeeShop db

--- inventory-cream.php
+++ inventory-cream.php
@@ -1,4 +1,21 @@
 <!DOCTYPE html>
+
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// Database access configuration
+$config["dbuser"] = "ora_miaodan";			// change "cwl" to your own CWL
+$config["dbpassword"] = "a92389279";	// change to 'a' + your student number
+$config["dbserver"] = "dbhost.students.cs.ubc.ca:1522/stu";
+$db_conn = NULL;	// login credentials are used in connectToDB()
+
+$success = true;	// keep track of errors so page redirects only if there are no errors
+
+$show_debug_alert_messages = False; // show which methods are being triggered (see debugAlertMessage())
+?>
+
 <html lang="en">
 
 <head>
@@ -20,13 +37,13 @@
         <div class="category">
 
 
-            <a href="inventory-coffee.html"> Coffee </a>
+            <a href="inventory-coffee.php"> Coffee </a>
             <br>
-            <a href="inventory-cream.html"> Cream </a>
+            <a href="inventory-cream.php"> Cream </a>
             <br>
-            <a href="inventory-sweetener.html"> Sweetener </a>
+            <a href="inventory-sweetener.php"> Sweetener </a>
             <br>
-            <a href="inventory-toppings.html"> Toppings </a>
+            <a href="inventory-toppings.php"> Toppings </a>
         </div>
 
         <div class="inventory">
---
+++ inventory-sweetener.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// Database access configuration
+$config["dbuser"] = "ora_miaodan";			// change "cwl" to your own CWL
+$config["dbpassword"] = "a92389279";	// change to 'a' + your student number
+$config["dbserver"] = "dbhost.students.cs.ubc.ca:1522/stu";
+$db_conn = NULL;	// login credentials are used in connectToDB()
+
+$success = true;	// keep track of errors so page redirects only if there are no errors
+
+$show_debug_alert_messages = False; // show which methods are being triggered (see debugAlertMessage())
+?>
+
+<html lang="en">
+
+<head>
+  <title>Inventory</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <div id="top">
+    <a href="">Recipes</a>
+    <a href="inventory-home.php">Inventory</a>
+    <a href="">Order</a>
+  </div>
+
+  <br>
+
+  <div class="container">
+
+    <div class="category">
+
+
+        <a href="inventory-coffee.php"> Coffee </a>
+        <br>
+        <a href="inventory-cream.php"> Cream </a>
+        <br>
+        <a href="inventory-sweetener.php"> Sweetener </a>
+        <br>
+        <a href="inventory-toppings.php"> Toppings </a>
+    </div>
+
+    <div class="inventory">
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>Quantity</th>
+          <th>Supplier</th>
+        </tr>
+        <tr>
+          <td>Honey</td>
+          <td>Costco</td>
+          <td>1234 Main St.</td>
+        </tr>
+        <tr>
+          <td>Vanilla Syrup</td>
+          <td>Superstore</td>
+          <td>9955 Oak St.</td>
+        </tr>
+      </table>
+    </div>
+  </div>
+</body>
+</html>
